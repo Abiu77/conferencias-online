8746e8872a043a8fc0aa0172f67a8dd9afca9e1c
Eli Abiu Condori Quispe <elicondoriquispe860@gmail.com>
2024-06-12 07:34:05 -0500

    Agregaron ultima tabla EVENTOS_REGISTRO

--- models/data.sql
+++ models/data.sql
@@ -129,3 +129,14 @@ INSERT INTO `regalos` (`id`, `nombre`) VALUES
 (7, 'Camisa Hombre - Mediana'),
 (8, 'Camisa Hombre - Grande'),
 (9, 'Camisa Hombre - XL');
+
+CREATE TABLE `eventos_registros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `evento_id` int DEFAULT NULL,
+  `registro_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `evento_id` (`evento_id`),
+  KEY `registro_id` (`registro_id`),
+  CONSTRAINT `eventos_registros_ibfk_1` FOREIGN KEY (`evento_id`) REFERENCES `eventos` (`id`),
+  CONSTRAINT `eventos_registros_ibfk_2` FOREIGN KEY (`registro_id`) REFERENCES `registros` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
